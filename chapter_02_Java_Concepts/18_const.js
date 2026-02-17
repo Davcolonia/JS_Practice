8e35cbb5e7b095c936ada324aa540a3d7215309c
Pramod Dutta <pramoddutta_96@yahoo.com>
2026-02-17 08:26:39 +0530

    Update code for JS concepts

--- chapter_02_Java_Concepts/18_const.js
+++ chapter_02_Java_Concepts/18_const.js
@@ -9,3 +9,6 @@ const pi = 3.14;
 const prod_api_url = "https://app.vwo.com/#login";
 //prod_api_url = "https://google.com"
 const qa_api_url = "https://qa.vwo.com/#login";
+
+let abc = "anil";
+console.log(abc);

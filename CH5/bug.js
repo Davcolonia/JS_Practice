f6c37c1d8c8c93964063a9bd243aa3a5a5d9487e
Davcolonia <39289038+Davcolonia@users.noreply.github.com>
2026-02-27 22:21:04 -0800

    commit

---
+++ CH5/bug.js
@@ -0,0 +1,13 @@
+let fruit = "banana";
+
+switch (fruit) {
+    case "apple":
+        console.log("Apple");
+    case "banana":
+        console.log("Banana");
+    case "orange":
+        console.log("Orange");
+    default:
+        console.log("Unknown");
+}
+

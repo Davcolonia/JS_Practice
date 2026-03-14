4e3a25ad1efbdbbe7c7df34a0f49d8d092dd7d0d
Davcolonia <39289038+Davcolonia@users.noreply.github.com>
2026-03-14 00:48:09 -0700

    push before exam

--- PlaywrightClasses/CH7/ARRAY1.js
+++ PlaywrightClasses/CH7/ARRAY1.js
@@ -12,16 +12,22 @@ console.log(arr)
 
 
 let nums = [10,1,3,2,21,2];
+let nums2 = [0,1,2]
 nums.sort()
 console.log(nums);
 nums.sort((a,b) => a-b);
 console.log(nums);
 nums.sort((a,b) => b-a);
 console.log(nums);
+console.log(nums[0]);
+console.log(Array.isArray(nums));
+console.log(nums2.length);
+nums2.push(10)
+console.log(nums2);
+console.log("two".length)
 
-
-let suite1 = [{ name: "login", status: "pass" }];
-// let suite2 = [...suite1];
-let suite2 = Array.from(suite1);
-suite2[0].status = "fail";
-console.log(suite1[0].status);
+// let suite1 = [{ name: "login", status: "pass" }];
+// // let suite2 = [...suite1];
+// let suite2 = Array.from(suite1);
+// suite2[0].status = "fail";
+// console.log(suite1[0].status);
---
+++ PlaywrightClasses/CH9/ARRAYS78.js
@@ -0,0 +1,9 @@
+const greet = function (name1){
+    return `Hello, ${name1}!`;
+}
+
+console.log(greet("Mi"));
+
+const greet2 = (name1) => `Hello, ${name1}!`;
+console.log(greet2("wi"));
+

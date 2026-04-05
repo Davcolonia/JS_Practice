9d49b0463fc4228d17e92f7d56d8d48a31ce553e
Davcolonia <39289038+Davcolonia@users.noreply.github.com>
2026-04-05 13:14:49 -0700

    add test question

--- tasks/test2_5.js
+++ tasks/test2_5.js
@@ -1,2 +1,16 @@
 //Reusable Step Runner with Callback
 //Write a higher-order function `runStep(stepName, actionFn)` that logs the start of a step, executes the callback, and returns an object in the format `{ stepName, passed, message }`. If the callback throws an error, catch it and return `passed: false` with the error message.
+
+function runStep(stepName, actionFn){
+
+    const result = actionFn();
+
+return{
+    StepName: stepName,
+    passed: true,
+    message: result ?? "Step completed successfully"
+};
+
+
+
+}

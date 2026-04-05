46eb74e149c2d7c50f96bcad584d2def88a01be8
Davcolonia <39289038+Davcolonia@users.noreply.github.com>
2026-04-05 13:09:29 -0700

    adjust test answer

--- tasks/test2_2.js
+++ tasks/test2_2.js
@@ -1,12 +1,13 @@
 function users(names, role){
-const test_user = {
-username: [],
-email: [],
-role: []
-};
+const test_user = [];
 for(i in names){
+    const userName = names[i].toLowerCase().replaceAll(" ", "_");
+ test_user.push({
+      username: username,
+      email: username + "@playwrightbatch.com",
+      role: roles[i]
+    });
+  }
 
-
-}
-
+  return test_user;
 }
